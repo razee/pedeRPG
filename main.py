c1e06b69b154d6a04f54262c0f91e29e46519fb1
JernejB <jernej@coderdojo.si>
2013-09-13 15:50:45 +0200

    Created: Player class

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ class Game:
         
         self.RESOLUTION = 1024, 768
 
-        BALL_RADIUS = 10
-        self.ball = sf.CircleShape()
-        self.ball.radius = BALL_RADIUS - 3
-        self.ball.outline_thickness = 3
-        self.ball.outline_color = sf.Color.BLACK
-        self.ball.fill_color = sf.Color.WHITE
-        self.ball.origin = (BALL_RADIUS / 2, BALL_RADIUS / 2)
-        self.ball.position = ((self.RESOLUTION[0]/2, self.RESOLUTION[1]/2))
+        self.playerOne = Player(self)
+
+        # BALL_RADIUS = 10
+        # self.ball = sf.CircleShape()
+        # self.ball.radius = BALL_RADIUS - 3
+        # self.ball.outline_thickness = 3
+        # self.ball.outline_color = sf.Color.BLACK
+        # self.ball.fill_color = sf.Color.WHITE
+        # self.ball.origin = (BALL_RADIUS / 2, BALL_RADIUS / 2)
+        # self.ball.position = ((self.RESOLUTION[0]/2, self.RESOLUTION[1]/2))
 
 
         TITLE_TEXT = "pedeRPG"
@@ -33,16 +35,33 @@ class Game:
                 if event == sf.CloseEvent:
                     self.window.close()
                 elif sf.Keyboard.is_key_pressed(sf.Keyboard.W):
-                    self.ball.move((0, -10)) 
+                    self.playerOne.movePlayer(0, -10) 
                 elif sf.Keyboard.is_key_pressed(sf.Keyboard.S):
-                    self.ball.move((0, 10))
+                    self.playerOne.movePlayer(0, 10)
                 elif sf.Keyboard.is_key_pressed(sf.Keyboard.A):
-                    self.ball.move((-10, 0))
+                    self.playerOne.movePlayer(-10, 0)
                 elif sf.Keyboard.is_key_pressed(sf.Keyboard.D):
-                    self.ball.move((10, 0))
+                    self.playerOne.movePlayer(10, 0)
             self.window.clear(sf.Color.TRANSPARENT)
-            self.window.draw(self.ball)
+            self.window.draw(self.playerOne.shape)
             self.window.display()
+
+class Player:
+
+    def __init__(self, GameInstanceClass):
+        # Player default is a ball
+        self.size = 10 # For ball shape size = radius = diameter/2
+        self.shape = sf.CircleShape()
+        self.shape.radius = self.size -3
+        self.shape.outline_thickness = 3
+        self.shape.outline_color = sf.Color.BLACK
+        self.shape.fill_color = sf.Color.WHITE
+        self.shape.origin = (self.size / 2, self.size/2)
+        self.shape.position = (GameInstanceClass.RESOLUTION[0]/2, GameInstanceClass.RESOLUTION[1]/2)
+
+    def movePlayer(self,x,y):
+        self.shape.move((x,y))
+
 
 if __name__ == "__main__":
     myapp=Game()
